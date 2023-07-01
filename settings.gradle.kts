0d81adf7dad8090eac9ed6972c2c110622a3d92d
Julian <42209193+juliarn@users.noreply.github.com>
2023-07-01 17:21:33 +0200

    Update OptiFine 1.20.1 to HD U I5 pre7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "optifine"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.22"
+    val labyGradlePluginVersion = "0.3.26"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
